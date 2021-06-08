1a4fcba09750e1ff56b1e3fde66906b9dadf6881
Takashi Kawasaki <espresso3389@gmail.com>
2021-06-08 16:56:25 +0900

    0.3.4

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# 0.3.4
+
+* Support [untrusted workspaces](https://github.com/microsoft/vscode/issues/120251)
--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,19 @@
-// genuuid - Generate UUID
-// (C) 2016 Takashi Kawasaki (@espresso3389)
 'use strict';
+
 import * as vscode from 'vscode';
-let uuid = require('node-uuid');
+import { v4 as uuidv4 } from 'uuid';
 
 function registerGenerator(context: vscode.ExtensionContext, name: string, random: () => string) {
     let disposable = vscode.commands.registerCommand(name, () => {
         let editor = vscode.window.activeTextEditor;
         if (editor)
-            editor.edit(edit => editor.selections.forEach(v => edit.replace(v, random())));
+            editor.edit(edit => editor!.selections.forEach(v => edit.replace(v, random())));
     });
     context.subscriptions.push(disposable);
 }
 
 export function activate(context: vscode.ExtensionContext) {
-    registerGenerator(context, 'genuuid.generateUUID', () => uuid.v4());
+    registerGenerator(context, 'genuuid.generateUUID', () => uuidv4());
 }
 
 export function deactivate() {
